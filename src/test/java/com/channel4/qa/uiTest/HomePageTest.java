f748032d0f22b925dec0c46f62e04a486fbdfc00
vikasEAI <vikas.kumar@engineer.ai>
2021-03-25 23:00:12 +0530

    Test case package created

---
+++ src/test/java/com/channel4/qa/uiTest/HomePageTest.java
@@ -0,0 +1,12 @@
+package com.channel4.qa.uiTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+
+    @Test
+    public void launchApp(){
+        Reporter.log("Success", true);
+    }
+}
